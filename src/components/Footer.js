e8cdb845d484c0aed95269ef8b5f8793dc11c60f
dmantilla17 <derekaraujo11@gmail.com>
2023-04-12 22:16:22 -0400

    imported footer and header

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="App-footer">
+      <ul id="footlinks">
+        <li className="footlink">
+          <a href="https://github.com/dmantilla17">Github</a>
+        </li>
+        <li className="footlink">
+          <a href="https://www.linkedin.com">LinkedIn</a>
+        </li>
+        <li className="footlink">
+          <a href="https://youtu.be/LrSX_OcpeJg">YouTube</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1> Derek's Portfolio</h1>
+    </header>
+  );
+};
+export default Header;
